7b767e01a277cf7d681fc012b62af01cb38228e4
Freshcoder335 <pow21016@byui.edu>
2023-03-07 13:14:25 -0800

    Menu Finished

--- prove/Develop05/SpamMenu.cs
+++ prove/Develop05/SpamMenu.cs
@@ -6,23 +6,42 @@ Authors:
 	Alvaro Nunez
 	Logan Clark
 
-Date:
+Date: 3/7/23
 
 Description:
-Responsibilities:
-
+Responsibilities: Return selected option
 
 Attributes:
-
+_Options
+list of options
 
 Behaviors:
-
+Return user selected option
 */
-
-public class SpamMenu
+using System;
+public class SPAMMenu
 {
-  public SpamMenu()
+
+  public SPAMMenu()
   {
 
+  }
+
+  public void SPAMmenuSelection()
+  {
+    Console.WriteLine("Menu Options:");
+    Console.WriteLine("1).  Create New Goal");
+    Console.WriteLine("2).  List Goals");
+    Console.WriteLine("3).  Save Goals");
+    Console.WriteLine("4).  Load Goals");
+    Console.WriteLine("5).  Record Event");
+    Console.WriteLine("6).  Quit");
+  }
+
+
+  public string SPAMselection()
+  {
+    Console.Write("Please select the numerical value of the choice you wish to proceed with: ");
+    return Console.ReadLine();
   }
 }
