c5fd5453088f0796a406df47a4600104f31269ad
Freshcoder335 <pow21016@byui.edu>
2023-03-07 13:48:52 -0800

    Checklist finished

--- prove/Develop05/SpamChecklistGoal.cs
+++ prove/Develop05/SpamChecklistGoal.cs
@@ -6,7 +6,7 @@ Authors:
 	Alvaro Nunez
 	Logan Clark
 
-Date:
+Date: 3/7/23
 
 Description:
 Responsibilities:
@@ -18,11 +18,41 @@ Attributes:
 Behaviors:
 
 */
-
-public class SpamChecklistGoal
+using System;
+public class SpamChecklistGoal: SpamParentGoal
 {
-  public SpamChecklistGoal()
+  private int _SPAMtimesTotal;
+  private int _SPAMbonus;
+  private int _SPAMtimesDone;
+  public SpamChecklistGoal(string _SpamName, string _SpamDescription, int _SpamScore, int SPAMtimesTotal, int SPAMbonus ):base (_SpamName, _SpamDescription, _SpamScore)
   {
-
+    _SPAMtimesTotal = SPAMtimesTotal;
+    _SPAMbonus = SPAMbonus;
+    _SPAMtimesDone = 0;
   }
+
+    public override string SpamToString()
+    {
+      if ( base.SpamGetIsComplete() == true){
+      return $"[X] {base._spamName} ({base._spamDescription} -- Currently Completed: {_SPAMtimesDone}/{_SPAMtimesTotal})";
+    } 
+    
+    else{
+      return $"[] {base._spamName} ({base._spamDescription}  -- Currently Completed: {_SPAMtimesDone}/{_SPAMtimesTotal})";
+    }
+    }
+
+    public override int SpamRecordEvent()
+    {
+      _SPAMtimesDone += 1;
+      if (_SPAMtimesDone == _SPAMtimesTotal)
+      {
+        base._spamIsComplete = true;
+        return _SPAMbonus;
+      }
+      else 
+      {
+        return base._spamPoints;
+      }
+    }
 }
