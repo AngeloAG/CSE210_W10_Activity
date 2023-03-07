879a73e8764806a13c40d7a44612d32164f7624a
Logan Clark <cla20067@byui.edu>
2023-03-07 13:37:20 -0700

    Finished Eternal Goal

--- prove/Develop05/SpamEternalGoal.cs
+++ prove/Develop05/SpamEternalGoal.cs
@@ -6,23 +6,40 @@ Authors:
 	Alvaro Nunez
 	Logan Clark
 
-Date:
+Date: 3/7/2023
 
 Description:
-Responsibilities:
-
+Responsibilities: Create a Goal that pertains the Religious Goals such as temple attendence, reading scriputres, etc.
 
 Attributes:
+Name of Goal
+short description
+Amount of pints
+Times to be accomplished
+Amount of points bonus
 
-
-Behaviors:
+Behaviors: Track and save religious goals and their respective points associtated with each Goal.
 
 */
 
-public class SpamEternalGoal
+public class SpamEternalGoal : SpamParentGoal
 {
-  public SpamEternalGoal()
+
+
+  public SpamEternalGoal(string _spamName, string _spamDescription, int _spamPoints) : base(_spamName, _spamDescription, _spamPoints)
   {
 
+  }
+
+  public override string SpamToString()
+  {
+    if(base.SpamGetIsComplete() == true)
+    {
+      return string.Format("[x] {0} ({1})", _spamName, _spamDescription);
+    }
+    else
+    {
+      return string.Format("[ ] {0} ({1})", _spamName, _spamDescription);
+    }
   }
 }
