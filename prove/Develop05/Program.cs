ddad1242acb680e2fded867c06e93b6e060533bd
AngeloAG <angelo.ag.079@gmail.com>
2023-03-09 12:41:35 -0700

    Partial implementation main

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -25,6 +25,51 @@ class Program
 {
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello Develop05 World!");
+    Console.Clear();
+    int spamPoints = 0;
+    List<SpamParentGoal> spamGoals = new List<SpamParentGoal>();
+    SPAMMenu spamMenu = new SPAMMenu();
+    bool spamKeepRunning = true;
+
+    while (spamKeepRunning)
+    {
+      Console.Clear();
+      SpamPrint("SPAM Goal Tracker");
+      SpamPrint("");
+      spamMenu.SPAMmenuSelection();
+      String spamChoice = spamMenu.SPAMselection();
+      switch (spamChoice)
+      {
+        case "1":
+          break;
+        case "2":
+          break;
+        case "3":
+          break;
+        case "4":
+          break;
+        case "5":
+          break;
+        case "6":
+          spamKeepRunning = false;
+          break;
+        default:
+          SpamPrint("Invalid input. Please select one of the options");
+          break;
+      }
+    }
+    Console.Clear();
+    SpamPrint("Thanks for using this program");
+
+  }
+
+  static void SpamPrint(String text)
+  {
+    Console.WriteLine(text);
+  }
+
+  static String SpamRead()
+  {
+    return Console.ReadLine();
   }
 }
