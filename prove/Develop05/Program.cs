85048224bdd4c60647017051be0408ff70d5e42f
AngeloAG <angelo.ag.079@gmail.com>
2023-03-09 12:59:29 -0700

    Added part of adding goals and displaying them

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -41,16 +41,68 @@ class Program
       switch (spamChoice)
       {
         case "1":
+          //Create new goal
+          spamMenu.SPAMsubMenu();
+          spamChoice = spamMenu.SPAMsubSelection();
+          String spamGoalName = "";
+          String spamGoalDesc = "";
+          int spamGoalPoints = 0;
+          int spamGoalBonus = 0;
+          int spamGoalTimesTotal = 0;
+
+          SpamPrintNoBreak("What is the name of the goal? ");
+          spamGoalName = SpamRead();
+          SpamPrint("");
+          SpamPrintNoBreak("What is a short description of it? ");
+          spamGoalDesc = SpamRead();
+          SpamPrint("");
+          SpamPrintNoBreak("What is the amount of points associated with this goal? ");
+          spamGoalPoints = int.Parse(SpamRead());
+          switch (spamChoice)
+          {
+            case "1":
+              // Simple Goal
+              SpamSimpleGoal spamSimpleGoal = new SpamSimpleGoal(spamGoalName, spamGoalDesc, spamGoalPoints);
+              spamGoals.Add(spamSimpleGoal);
+              break;
+            case "2":
+              // Eternal Goal
+              SpamEternalGoal spamEternalGoal = new SpamEternalGoal(spamGoalName, spamGoalDesc, spamGoalPoints);
+              spamGoals.Add(spamEternalGoal);
+              break;
+            case "3":
+              // Checklist Goal
+              SpamPrint("");
+              SpamPrintNoBreak("How many times does this goal need to be accomplished for a bonus? ");
+              spamGoalBonus = int.Parse(SpamRead());
+              SpamPrint("");
+              SpamPrintNoBreak("What is the bonus for accomplishing it that many times? ");
+              spamGoalTimesTotal = int.Parse(SpamRead());
+
+              SpamChecklistGoal spamChecklistGoal = new SpamChecklistGoal(spamGoalName, spamGoalDesc, spamGoalPoints, spamGoalTimesTotal, spamGoalBonus);
+              spamGoals.Add(spamChecklistGoal);
+              break;
+          }
           break;
         case "2":
+          // List goals
+          SpamPrint("The goals are:");
+          foreach (SpamParentGoal spamGoal in spamGoals)
+          {
+            SpamPrint(spamGoal.SpamToString());
+          }
           break;
         case "3":
+          // Save Goals
           break;
         case "4":
+          // Load Goals
           break;
         case "5":
+          // Record Event
           break;
         case "6":
+          // Quit
           spamKeepRunning = false;
           break;
         default:
@@ -66,6 +118,11 @@ class Program
   static void SpamPrint(String text)
   {
     Console.WriteLine(text);
+  }
+
+  static void SpamPrintNoBreak(String text)
+  {
+    Console.Write(text);
   }
 
   static String SpamRead()
