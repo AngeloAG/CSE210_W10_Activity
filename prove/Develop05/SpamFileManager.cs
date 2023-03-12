a3c8f7c1ee589f51eee50ed43596a0a92b80fc5e
Jeffrey Meldrum <agentdex2@gmail.com>
2023-03-11 17:25:07 -0700

    possibly finished the loading and saving. needs tested.

--- prove/Develop05/SpamFileManager.cs
+++ prove/Develop05/SpamFileManager.cs
@@ -30,28 +30,63 @@ public class SpamFileManager
 
   public List<List<string>> SpamFileLoader()
   {
+    List<List<string>> SpamLines = new List<List<string>>();
     string SpamFilename = "GoalsAndPoints.txt";
-    List<string> SpamLines = System.IO.File.ReadAllLines(SpamFilename);
+    string[] SpamLinesArray = System.IO.File.ReadAllLines(SpamFilename);
 
-    // reads the current information in the text document
-    foreach (string SpamLine in SpamLines)
+    // converts array into a list
+    foreach (string SpamLineArray in SpamLinesArray)
     {
-      List<string> SpamParts = SpamLine.Split("|");
-    }
+      List<string> SpamParts = new List<string>();
+      string[] SpamPartsArray = SpamLineArray.Split("|");
 
+      // splits the line into seperate parts then saves it into a list
+      foreach (string SpamPartArray in SpamPartsArray)
+      {
+        SpamParts.Add(SpamPartArray);
+      }
+      
+      // adds the part list to the lines list
+      SpamLines.Add(SpamParts);
+    }
     return SpamLines;
   }
 
   public void SpamFileSaver(List<string> SpamGoalList, int SpamPointsTotal)
   {
+    // reads the text document and converts it into a list
     string SpamFilename = "GoalsAndPoints.txt";
+    string[] SpamLinesArray = System.IO.File.ReadAllLines(SpamFilename);
+    List<string> SpamLines = new List<string>();
+    foreach (string SpamLineArray in SpamLinesArray)
+    {
+      SpamLines.Add(SpamLineArray);
+    }
 
-  // Loads what is currently stored into the txt document
-    List<List<string>> SpameLoadFile = SpamFileLoader();
+    // Loads what is currently stored into the txt document
+    string SpamPointsTotalString = SpamPointsTotal.ToString();
 
+    // updates the point total
+    SpamLines[0] = SpamPointsTotalString;
+
+    // adds on to the previously made goals
+    foreach (string SpamGoal in SpamGoalList)
+    {
+      SpamLines.Add(SpamGoal);
+    }
+
+// clears the text document
+    TextWriter SpamClear = new StreamWriter(SpamFilename, false);
+    SpamClear.Write(string.Empty);
+    SpamClear.Close();
+
+// writes the goals on the txt document
     using (StreamWriter SpamOutputFile = new StreamWriter(SpamFilename))
     {
-      
+      foreach (string SpamLine in SpamLines)
+      {
+        SpamOutputFile.WriteLine($"{SpamLine}");
+      }
     }
   }
 }
